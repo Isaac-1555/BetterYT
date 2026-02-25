a576cdcee75999b99b07daa1a5f7e192e023298d
Isaac Daniel <benisaac1892@gmail.com>
2026-02-24 23:40:08 -0700

    Fixed the padding issue

--- assets/index.tsx-loader-B7BvjlZj.js
+++ assets/index.tsx-loader-B7BvjlZj.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/index.tsx-l5Q8KIuu.js")
+      chrome.runtime.getURL("assets/index.tsx-nKlu_8bF.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
